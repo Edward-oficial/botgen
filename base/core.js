17dfcd3482843c63efa984a02894c528f7abf231
Edward <cololacalempira5@gmail.com>
2026-08-09 18:30:07 -0600

    Actualizar core.js

--- base/core.js
+++ base/core.js
@@ -3,7 +3,7 @@ import makeWASocket, {
   fetchLatestBaileysVersion,
   DisconnectReason,
   Browsers,
-} from "baileysx";
+} from "baileysxz";
 import { Boom } from "@hapi/boom";
 import pino from "pino";
 import chalk from "chalk";
